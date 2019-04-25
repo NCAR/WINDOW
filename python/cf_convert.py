b2dad4905c17f34080dd76ceb4f3e59654503215
hsoh-u <hsoh@ucar.edu>
2019-04-25 19:22:46 +0000

    Insert GIS attributes

--- python/cf_convert.py
+++ python/cf_convert.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import os, sys
 import json
+import glob
 from optparse import OptionParser
 
 from netCDF4 import Dataset as nc4_Dataset
@@ -12,9 +14,11 @@ from cfchecks import getargs as check_getargs
 WINDOW_CF_NAMES_CFG='cf_names.json'
 
 DEBUG_P  = "   DEBUG]"
-INFO_P   = "   INFO]"
-ACTION_P = "   ACTION]"
-FIXME_P  = "   == FIX ME =="
+ERROR_P  = "   ERROR:"
+INFO_P   = "    INFO]"
+WARN_P   = "    WARN:"
+ACTION_P = "  ACTION]"
+FIXME_P  = "  == FIX ME =="
 
 def create_parser():
     usage_str = "%prog [options] "
@@ -23,7 +27,7 @@ def create_parser():
     #        help=" output name. must have nc extension, optional")
     parser.add_option("--out_dir", "--out-dir", dest="out_dir", default='.',
             help=" output directory - optional")
-    parser.add_option("--geogird", "--out-dir", dest="geogird", default=None,
+    parser.add_option("--geogrid", "--geogrid-nc", dest="geogrid_nc", default=None,
             help=" output directory - optional")
     parser.add_option('-a','--area_types', dest="areatypes", default=None)
     parser.add_option('-b','--badc', dest="badc", default=None)
@@ -67,11 +71,11 @@ def correct_variable_fatal(var_name, to_var, check_code, message, cf_name_map=No
             p=FIXME_P, v=var_name, c=check_code, m=message))
 
 def correct_variable_error(var_name, to_var, check_code, message, cf_name_map=None):
-    debug = False
+    #debug = False
     #debug = not debug
     if check_code == '3.1' and 0 == message.find('Units'):
         cf_units = cf_name_map.get_cf_units(var_name)
-        if units is not None:
+        if cf_units is not None:
             attr_key = 'units'
             to_var.setncattr('units', cf_units)
             print("{p} {k} attribute is changed to {v}".format(
@@ -128,7 +132,7 @@ def correct_variable_version(var_name, to_var, check_code, message, cf_name_map=
             p=FIXME_P, v=var_name, c=check_code, m=message))
     
 
-class cf_tools:
+class nc_tools:
 
     @staticmethod
     def copy_dimensions(from_nc, to_nc):
@@ -141,8 +145,8 @@ class cf_tools:
         #print('to_nc_dims', to_nc_dims)
         return to_nc_dims
         
+    @staticmethod
     def copy_global_attributes(from_nc, to_nc):
-        to_nc_dims = []
         for attr_name in from_nc.ncattrs():
             #print('\t%s:' % attr_name, repr(from_nc.getncattr(attr_name)))
             to_nc.setncattr(attr_name, from_nc.getncattr(attr_name))
@@ -155,11 +159,12 @@ class cf_tools:
             to_var.setncattr(attr_name, from_var.getncattr(attr_name))
         return to_var
         
+    @staticmethod
     def create_nc_from(nc_name, from_nc):
         out_nc = nc4_Dataset(nc_name, "w")
         
-        to_nc_dims = cf_tools.copy_dimensions(from_nc, out_nc)
-        cf_tools.copy_global_attributes(from_nc, out_nc)
+        nc_tools.copy_dimensions(from_nc, out_nc)
+        nc_tools.copy_global_attributes(from_nc, out_nc)
         return out_nc
         
 
@@ -235,10 +240,51 @@ class CF_Corrector:
     }
     
     def __init__(self):
-        self.geogrid = None
+        self.gis_attrs = {}
         self.cf_name_map = CF_Names()
     
-    def correct(self, from_nc_name, results, to_nc_name, categories):
+    def add_GIS_attrs(self, to_var):
+        nc_dims = to_var.get_dims()
+        if 1 >= len(nc_dims):
+            return
+        
+        nc_attr_names = to_var.ncattrs()
+        
+        attr_key = 'long_name'
+        if attr_key in nc_attr_names: 
+            nc_attr = to_var.getncattr(attr_key)
+            if nc_attr is not None:
+                if nc_attr == 'times':
+                    return
+        
+        attr_key = 'standard_name'
+        if attr_key in nc_attr_names: 
+            nc_attr = to_var.getncattr(attr_key)
+            if nc_attr is not None:
+                if nc_attr == 'time':
+                    return
+        
+        attr_key = 'coordinates'
+        has_coordinates_attr = False
+        if attr_key in nc_attr_names: 
+            nc_attr = to_var.getncattr(attr_key)
+            has_coordinates_attr= (nc_attr is not None)
+
+        gis_attrs = self.gis_attrs
+        if 0 < len(gis_attrs):
+            for attr_key in gis_attrs.keys():
+                nc_key = attr_key[len('WINDOW_'):]
+                if nc_key in nc_attr_names:
+                    continue
+                
+                if nc_key != 'esri_pe_string':
+                    if has_coordinates_attr:
+                        continue
+                
+                to_var.setncattr(nc_key, gis_attrs[attr_key])
+                print("adding {k}={v}".format(k=nc_key, v=gis_attrs[attr_key]))
+        
+    def correct(self, from_nc_name, to_nc_name, results, categories):
         debug = False
         #debug = not debug
         global_results = results["global"]
@@ -249,7 +295,7 @@ class CF_Corrector:
         
         from_nc = nc4_Dataset(from_nc_name, "r")
         
-        to_nc   = cf_tools.create_nc_from(to_nc_name, from_nc)
+        to_nc   = nc_tools.create_nc_from(to_nc_name, from_nc)
         
         for category in categories:
             if category == 'VERSION':
@@ -260,22 +306,26 @@ class CF_Corrector:
                 self.correct_global(category_handler, from_nc, to_nc,
                         category_results)
         
+        gis_attrs = self.gis_attrs
         results_var_names = variables_results.keys()
         for var_name in from_nc.variables.keys():
             from_var = from_nc.variables[var_name]
-            to_var = cf_tools.copy_variable(to_nc, from_var)
-            if var_name not in results_var_names:
-                continue
-            cf_results = variables_results.get(var_name)
-            if cf_results is not None:
-                for category in categories:
-                    if category == 'VERSION':
-                        continue
-                    category_results = cf_results.get(category)
-                    if category_results is not None and 0 < len(category_results):
-                        category_handler = CF_Corrector.VARIABLE_HANDLERS[category]
-                        self.correct_variable(category_handler, var_name,
-                                to_var, category_results, self.cf_name_map)
+            to_var = nc_tools.copy_variable(to_nc, from_var)
+            
+            if var_name in results_var_names:
+                cf_results = variables_results.get(var_name)
+                if cf_results is not None:
+                    for category in categories:
+                        if category == 'VERSION':
+                            continue
+                        category_results = cf_results.get(category)
+                        if category_results is not None and 0 < len(category_results):
+                            category_handler = CF_Corrector.VARIABLE_HANDLERS[category]
+                            self.correct_variable(category_handler, var_name,
+                                    to_var, category_results, self.cf_name_map)
+            
+            if 0 < len(gis_attrs):
+                self.add_GIS_attrs(to_var)
         
     #def global_result_fatal(self, from_nc, results, to_nc):
     #    global_results = results["global"]
@@ -308,56 +358,84 @@ class CF_Corrector:
             message = result.strip()
         
         return (check_code, message)
-    def set_geogrid(self, geogrid):
-        self.geogrid = geogrid
+    def set_gis_attrs(self, gis_attrs):
+        self.gis_attrs = gis_attrs
 
-def make_output_names(in_files, output_dir):
-    input_index = 0
-    out_name_pattern = None
-    out_files = []
-    for file in in_files:
-        cur_output_name = os.path.basename(file)
-        if not cur_output_name.endswith(".nc"):
-            cur_output_name = cur_output_name + ".nc"
-        if output_dir is not None:
-            cur_output_name = os.path.join(output_dir, cur_output_name)
-        out_files.append(cur_output_name)
-    return out_files
+    #VARIABLE_IGNORE_CODES = {
+    #      '2.1': ["Filename must have .nc suffix"],
+    #    '2.6.1': ["No 'Conventions' attribute present"]
+
+class CF_aux_tools:
+
+    @staticmethod
+    def collect_gis_attributes(geogrid_name):
+        gis_attrs = {}
+        geogrid_nc = nc4_Dataset(geogrid_name, "r")
+        global_attrs = geogrid_nc.ncattrs()
+        for global_attr in global_attrs:
+            if global_attr.startswith('WINDOW_'):
+                gis_attrs[global_attr] = geogrid_nc.getncattr(global_attr)
+        grid_mapping_key = 'WINDOW_grid_mapping'
+        if grid_mapping_key in gis_attrs.keys():
+            grid_mapping_var = gis_attrs[grid_mapping_key]
+            
+        del geogrid_nc 
+        return gis_attrs
+        
+    @staticmethod
+    def make_output_names(in_files, output_dir):
+        out_files = []
+        for in_file in in_files:
+            output_name = os.path.basename(in_file)
+            if not output_name.endswith(".nc"):
+                output_name = output_name + ".nc"
+            if output_dir is not None:
+                output_name = os.path.join(output_dir, output_name)
+            out_files.append(output_name)
+        return out_files
+        
+    @staticmethod
+    def make_output_names_from_input_dir(in_dir, output_dir):
+        in_files = glob.glob(os.path.join(in_dir, '*'))
+        out_files = CF_aux_tools.make_output_names(in_files, output_dir)
+        return out_files
     
-def make_output_names_rev1(in_files, out_name, output_dir='.'):
-    input_index = 0
-    out_name_pattern = None
-    has_output_name = out_name is not None
-    if has_output_name and 1 <= len(in_files):
-        out_name_pattern = (out_name[0:-3] if out_name.endswith(".nc") else out_name) + "_{n}.nc"
-    out_files = []
-    for file in in_files:
-        cur_output_name = out_name
-        print("cur_output_name", cur_output_name)
-        if has_output_name:
-            if out_name_pattern is not None:
-                input_index += 1
-                cur_output_name = out_name_pattern.format(n=input_index)
-        else:
-            cur_output_name = os.path.basename(file)
-            if not cur_output_name.endswith(".nc"):
-                cur_output_name = cur_output_name + ".nc"
-        if output_dir is not None:
-            cur_output_name = os.path.join(output_dir, cur_output_name)
-        out_files.append(cur_output_name)
-    return out_files
+    # def make_output_names_rev1(in_files, out_name, output_dir='.'):
+    #     input_index = 0
+    #     out_name_pattern = None
+    #     has_output_name = out_name is not None
+    #     if has_output_name and 1 <= len(in_files):
+    #         out_name_pattern = (out_name[0:-3] if out_name.endswith(".nc") else out_name) + "_{n}.nc"
+    #     out_files = []
+    #     for file in in_files:
+    #         cur_output_name = out_name
+    #         print("cur_output_name", cur_output_name)
+    #         if has_output_name:
+    #             if out_name_pattern is not None:
+    #                 input_index += 1
+    #                 cur_output_name = out_name_pattern.format(n=input_index)
+    #         else:
+    #             cur_output_name = os.path.basename(file)
+    #             if not cur_output_name.endswith(".nc"):
+    #                 cur_output_name = cur_output_name + ".nc"
+    #         if output_dir is not None:
+    #             cur_output_name = os.path.join(output_dir, cur_output_name)
+    #         out_files.append(cur_output_name)
+    #     return out_files
     
 def main():
 
     parser = create_parser()
     (options, args) = parser.parse_args()
+    
+    #Filter arguments whcih are not for cfchecks
     my_args = []
     skip_next = False
     for arg in sys.argv[:]:
         if skip_next:
             skip_next = False
             continue
-        if arg in ("-o", "--output-name", "--out_dir", "--out-dir"):
+        if arg in ("-o", "--output-name", "--out_dir", "--out-dir", "--geogrid", "--geogrid-nc"):
             skip_next = True
             continue
         my_args.append(arg)
@@ -370,16 +448,27 @@ def main():
                         badc=badc, coards=coards, cfStandardNamesXML=standardName,
                         cfAreaTypesXML=areaTypes, version=version, debug=debug)
                         
-    out_files = make_output_names(in_files, options.out_dir)
+    out_files = CF_aux_tools.make_output_names(in_files, options.out_dir)
+    if options.geogrid_nc is None:
+        print("{p} The geogrid file {g} is missing. Please set it with --geogrid".format(
+                p=ERROR_P))
+        sys.exit(-1)
+    else:
+        if not os.path.exists(options.geogrid_nc):
+            print("{p} The geogrid file {g} does not exist. Ignored GIS attributes".format(
+                    p=WARN_P, g=options.geogrid_nc))
+        else:
+            gis_attrs = CF_aux_tools.collect_gis_attributes(options.geogrid_nc)
+            actor.set_gis_attrs(gis_attrs)
     for in_file, out_file in zip(in_files, out_files):
         #try:
             print("in_file, out_file", in_file, out_file)
             results = checker.checker(in_file)
-            actor.correct(in_file, results, out_file, checker.categories)
+            actor.correct(in_file, out_file, results, checker.categories)
         #except ex:
         #    print("Processing of file %s aborted due to error" % in_file)
         #    print("ex", ex)
-
+    
     totals = checker.get_total_counts()
 
     if debug:
@@ -392,9 +481,9 @@ def main():
     if errs:
         sys.exit(errs)
     
-    warns = totals["WARN"]
-    if warns:
-        sys.exit(-warns)
+    #warns = totals["WARN"]
+    #if warns:
+    #    sys.exit(-warns)
 
     sys.exit(0)
 
